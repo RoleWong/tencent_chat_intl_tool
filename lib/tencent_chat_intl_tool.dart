47fc1d04faaeeec80a3dca1973ad812d4bacba24
RoleWong <814209329@qq.com>
2023-09-14 16:35:59 +0800

    feat: Imp Usage

--- lib/tencent_chat_intl_tool.dart
+++ lib/tencent_chat_intl_tool.dart
@@ -3,7 +3,7 @@ library tencent_chat_intl_tool;
 import 'package:flutter/widgets.dart';
 import 'package:tencent_chat_intl_tool/localizations/tencent_cloud_chat_localizations.dart';
 
-TencentCloudChatLocalizations? tL10n = TencentChatIntlTool.localization;
+TencentCloudChatLocalizations tL10n = TencentChatIntlTool.localization!;
 
 class TencentChatIntlTool{
   static TencentCloudChatLocalizations? localization;
